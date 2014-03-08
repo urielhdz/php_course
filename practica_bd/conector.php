97ee5ec45ca811f2338aeb418bc64ac773d9d7a0
Uriel Hernández <uriel.hdzc@gmail.com>
2014-03-08 13:34:36 -0600

    Archivos terminados dia 2

--- practica_bd/conector.php
+++ practica_bd/conector.php
@@ -2,19 +2,39 @@
 	class Conector
 	{
 		private $host = "";
-		private $puerto = "";
+		private $database = "";
 		private $username = "";
 		private $password = "";
-
-		function __construct($host,$puerto,$username,$password)
+		private $mysqli = "";
+		function __construct($host,$database,$username,$password)
 		{	
 			$this->host = $host;
-			$this->puerto = $puerto;
+			$this->database = $database;
 			$this->username = $username;
 			$this->password = $password;
+			// backend_course_db
+			// mysqli_connect(host,username,password,database)
+			$this->mysqli = mysqli_connect($this->host,$this->username,$this->password,$this->database);
+			if ($this->mysqli->connect_errno) {
+			    echo "Failed to connect to MySQL: " . $this->mysqli->connect_errno;
+			}
 		}
-		public function conectar(){
-			echo "SOME NOISE";
+		public function select($table){
+			$res = $this->mysqli->query("SELECT * from ".$table);
+			
+			return $res->fetch_assoc();;
+		}
+		public function insert($table,$data){
+			$values = "";
+			$keys = "";
+			foreach ($data as $key => $value) {
+				$keys .= $key.",";
+				$values .= "'".$value."',";
+			}
+			$values = substr($values, 0, -1);
+			$keys = substr($keys, 0, -1);
+			return $this->mysqli->query("INSERT INTO ".$table."(".$keys.") VALUES(".$values.")");
+			
 		}
 		
 	}
